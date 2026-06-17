fbe7263d37f01b4346c29ea5ff0da4616daf7855
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-06-17 21:48:31 +0900

    wip: login

--- frontend-user/src/pages/Login.tsx
+++ frontend-user/src/pages/Login.tsx
@@ -1,14 +1,41 @@
 import { env } from "@/config/env.ts";
+import { useState, type SubmitEventHandler } from "react";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginHandler: SubmitEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`${env.apiBaseUrl}/auth/login`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const { user, token } = await response.json();
+    localStorage.setItem("token", token);
+  };
+
   return (
     <>
       <h2>Login</h2>
-      <form action={`${env.apiBaseUrl}/auth/login`} method="post">
+      <form onSubmit={loginHandler} method="post">
         <label htmlFor="username">username</label>
-        <input type="email" name="username" id="username" />
+        <input
+          type="email"
+          name="username"
+          id="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <label htmlFor="password">password</label>
-        <input type="password" name="password" id="password" />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">로그인</button>
       </form>
     </>
